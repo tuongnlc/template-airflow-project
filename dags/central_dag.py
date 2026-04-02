bf50aba9e2fe48f8db306391ff22a6c29dff81ac
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-04-02 14:57:50 +0700

    implement TaskFactory

--- dags/central_dag.py
+++ dags/central_dag.py
@@ -1,9 +1,12 @@
 from typing import Any
 from daglib.utils.config_loader import load_config
-
+from daglib.core.config import ConfigRegistry
 
 configs_path = "configs/ingestion_job"
 
 
 raw_configs: list[dict[str, Any]] = load_config(path=configs_path)
 config_registry = ConfigRegistry()
+config_registry.populate(raw_configs)
+
+task_factory_registry: TaskFactoryRegistry = TaskFactoryRegistry()
