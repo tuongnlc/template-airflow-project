b0592e148eb4b6a25bd6d5457d6a6c236a0e7a9a
tuong nguyen <tuongnguyen@tuongs-Mac-mini.local>
2026-04-02 15:15:06 +0700

    add task factory

--- dags/central_dag.py
+++ dags/central_dag.py
@@ -1,6 +1,10 @@
 from typing import Any
 from daglib.utils.config_loader import load_config
 from daglib.core.config import ConfigRegistry
+from daglib.core.task_factory import TaskFactoryRegistry
+from daglib.core.dummy import DummyTaskFactory
+
+
 
 configs_path = "configs/ingestion_job"
 
@@ -10,3 +14,4 @@ config_registry = ConfigRegistry()
 config_registry.populate(raw_configs)
 
 task_factory_registry: TaskFactoryRegistry = TaskFactoryRegistry()
+task_factory_registry.register(DummyTaskFactory())
